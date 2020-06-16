13419a56031cdbaaa93f3b9daa13bda70b2179dd
sfilmak <sfilmak@gmail.com>
2020-06-16 17:52:50 +0300

    Responsive list by @sidletsky

--- src/main/js/cars.js
+++ src/main/js/cars.js
@@ -1,26 +1,28 @@
 import React from 'react'
+import styled from "styled-components";
 import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
         overflow: 'hidden',
         backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-        marginLeft: '100px !important',
-        marginRight: '100px !important',
-        marginTop: '20px !important',
-        marginBottom: '20px !important',
     },
     gridSubtile: {
         background: 'rgba(0, 0, 0, 0.8)'
     },
     gridTile: {
-        display: 'block',
         cursor: 'pointer',
+        width: '400px',
+        height: '200px',
+        marginBottom: '20px',
+        "::marker": {
+            display: 'none'
+        }
     },
     title: {
         fontSize: '1.25rem',
@@ -29,6 +31,21 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '1rem',
     },
 }));
+
+const GridList = styled.div`
+    display: flex;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    flex-wrap: wrap;
+    list-style:none;
+    margin-right: 100px;
+    margin-left: 100px;
+    justify-content: space-around;
+    @media (min-width: 768px) {
+        margin-right: 0;
+        margin-left: 0;
+    }
+`;
 
 const Cars = ({ cars }) => {
     const classes = useStyles();
@@ -40,13 +57,13 @@ const Cars = ({ cars }) => {
 
     return (
         <div className={classes.root}>
-            <GridList cellHeight={245} cols={3} className={classes.gridList}>
+            <GridList>
                 {cars.map((car) => (
                     <GridListTile key={car.imageURL} className={classes.gridTile}  onClick={(e) => handleCarClick(e, car)}>
                         <img src={car.imageURL} alt={car.model} />
                         <GridListTileBar className={classes.gridSubtile}
-                            title={<b className={classes.title}>{car.manufacturer} {car.model}</b>}
-                            subtitle={<span className={classes.subtitle}>{car.pricePerDay}$/day</span>}
+                                         title={<b className={classes.title}>{car.manufacturer} {car.model}</b>}
+                                         subtitle={<span className={classes.subtitle}>{car.pricePerDay}$/day</span>}
                         />
                     </GridListTile>
                 ))}
