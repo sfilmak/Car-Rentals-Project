6ec7a5b0c91334d884a04334cd34ddbea309d9ef
sfilmak <sfilmak@gmail.com>
2020-06-19 15:53:15 +0300

    Data passing

--- src/main/js/CarInfo.js
+++ src/main/js/CarInfo.js
@@ -3,12 +3,19 @@ import React from 'react'
 const CarInfo = ({ cars }) => {
     let search = window.location.search;
     let params = new URLSearchParams(search);
-    let carID = params.get('id');
+    let carID = parseInt(params.get('id'));
+
     return (
         <div>
             <h2>CarID is {carID}</h2>
+            {cars.filter(function (car) {
+                return car.carID === carID
+            }).map((car) => (
+                <h2>Car model: {car.model}</h2>
+            ))}
         </div>
     );
 };
+
 
 export default CarInfo
